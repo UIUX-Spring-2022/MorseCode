384c6fc42e494ceddba9b5342c328ff23330a7be
Sophie Johnson <sophiejohnson@Sophies-MacBook-Air-5.local>
2022-04-14 17:12:32 -0400

    more initial commits

--- server.py
+++ server.py
@@ -5,3 +5,19 @@ from flask import Response, request, jsonify
 app = Flask(__name__)
 
 #insert data and everything here
+
+@app.route('/')
+def welcome():
+    return render_template('welcome.html')
+#we probably need to divide the templates for learn and quiz into learn_1, learn_2 etc based on different layouts for learning/quizzing
+
+@app.route('/edit/<id>')
+def learn(id):
+    return render_template('learn.html', id=id)
+
+@app.route('/quiz/<id>')
+def quiz(quiz_id):
+    return render_template('quiz.html', quiz_id=quiz_id)
+
+if __name__ == '__main__':
+   app.run(debug = True)
